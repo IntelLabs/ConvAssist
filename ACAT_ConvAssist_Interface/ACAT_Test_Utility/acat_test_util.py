37e9ee97525385088903b4045fa7576e309e4f48
Beale, Michael <michael.beale@intel.com>
2024-09-17 11:23:12 -0700

    hack fix due to change in message_handling

--- ACAT_ConvAssist_Interface/ACAT_Test_Utility/acat_test_util.py
+++ ACAT_ConvAssist_Interface/ACAT_Test_Utility/acat_test_util.py
@@ -1,10 +1,14 @@
 import sys
 import os
+import json
+from typing import Any
 
 if sys.platform == 'win32':
     try:
         import win32pipe
+        import win32event
         import win32file
+        import winerror
         import win32security
         import pywintypes
     except ImportError:
@@ -15,7 +19,7 @@ if current_path not in sys.path:
     sys.path.append(current_path)
 
 from ACAT_ConvAssist_Interface.ConvAssistCPApp.ACATMessageTypes import ConvAssistSetParam, ParameterType, ConvAssistMessage, ConvAssistMessageTypes, ConvAssistPredictionTypes
-from ACAT_ConvAssist_Interface.message_handler.Win32PipeHandler import get_incoming_message
+# from ACAT_ConvAssist_Interface.message_handler.Win32PipeHandler import get_incoming_message
 
 set_path_param = ConvAssistSetParam(ParameterType.PATH, "C:/Users/mbeale/source/repos/ConvAssist/ACAT_ConvAssist_Interface/ConvAssistCPApp/resources")
 set_static_path_param = ConvAssistSetParam(ParameterType.PATHSTATIC, "C:/Users/mbeale/source/repos/ConvAssist/ACAT_ConvAssist_Interface/ConvAssistCPApp/resources/static_resources") 
@@ -34,6 +38,70 @@ params = [set_log_param, set_enable_logs_param, set_suggestions_param, \
 
 # Define the named pipe
 pipe_name = r'\\.\pipe\ACATConvAssistPipe'
+
+def is_pipe_overlapped(pipe_handle) -> bool:
+        """
+        Check if the pipe is overlapped
+
+        :param pipe_handle: Handle of the pipe
+        :return: True if overlapped, False otherwise
+        """
+        try:
+            _, _, pipe_mode, _ = win32pipe.GetNamedPipeInfo(pipe_handle)
+            if pipe_mode & win32pipe.PIPE_NOWAIT:
+                return True
+            else:
+                return False
+
+        except pywintypes.error as e:
+            raise Exception(f"Error checking if pipe is overlapped: {e}") from e
+        
+def get_incoming_message(Pipehandle) -> Any:
+        """
+        Get the incoming message from the pipe
+
+        :param Pipehandle: Handle of the pipe
+        :return: message received
+        """
+        try:
+            buffer_size = 4096  # For example, 4 KB
+            if is_pipe_overlapped(Pipehandle.handle):
+                # Allocate a buffer for the data
+                read_buffer = win32file.AllocateReadBuffer(buffer_size)
+
+                # Create an OVERLAPPED structure
+                overlapped = pywintypes.OVERLAPPED()
+
+                # Create an event for the OVERLAPPED structure
+                overlapped.hEvent = win32event.CreateEvent(None, True, False, None)
+                
+                # Start the overlapped read operation
+                err_code, _ = win32file.ReadFile(Pipehandle.handle, read_buffer, overlapped)
+
+                # If the operation is pending, wait for it to complete
+                if err_code == winerror.ERROR_IO_PENDING:
+                    timeout = 100 # milliseconds
+                    win32event.WaitForSingleObject(overlapped.hEvent, timeout)
+                    
+                    # Get the result of the overlapped operation
+                    n_bytes_read = win32file.GetOverlappedResult(Pipehandle.handle, overlapped, True)
+
+                    # Extract the data from the buffer
+                    data = (bytes(read_buffer[:n_bytes_read]).decode('utf-8')) # type: ignore
+                
+                elif err_code == win32event.WAIT_TIMEOUT:
+                    raise TimeoutError("Timeout waiting for overlapped operation to complete")
+            
+            else:
+                # Read the data from the pipe
+                _, data =  win32file.ReadFile(Pipehandle.handle, buffer_size)
+            
+            # Load the data as json and return
+            return json.loads(data)
+        
+        except pywintypes.error as e:
+            raise BrokenPipeError(f"Error receiving message from named pipe: {e}") from e
+
 
 def create_named_pipe(pipe_name):
     """Create a named pipe with the given name."""
