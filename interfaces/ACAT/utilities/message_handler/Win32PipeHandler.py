d101cc93314f1e46d20afd5b2e57dc0003dd4bd4
Beale, Michael <michael.beale@intel.com>
2024-09-24 09:59:01 -0700

    removed overlapped check since it was never used.  Simplifield readFile.

--- interfaces/ACAT/utilities/message_handler/Win32PipeHandler.py
+++ interfaces/ACAT/utilities/message_handler/Win32PipeHandler.py
@@ -2,13 +2,11 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 import sys
-import time
 from typing import Any
 
 if sys.platform == "win32":
     import win32file
     import win32pipe
-    import win32event
     import pywintypes
     import winerror
 
@@ -58,74 +56,34 @@ else:
         def create_connection(self) -> None:
             pass
 
-        def _is_pipe_overlapped(self) -> bool:
-            """
-            Check if the pipe is overlapped
-
-            :param pipe_handle: Handle of the pipe
-            :return: True if overlapped, False otherwise
-            """
-            try:
-                assert self.pipe_handle is not None
-                _, _, pipe_mode, _ = win32pipe.GetNamedPipeInfo(self.pipe_handle.handle)
-                if pipe_mode & win32pipe.PIPE_NOWAIT:
-                    return True
-                else:
-                    return False
-
-            except pywintypes.error as e:
-                raise Exception(f"Error checking if pipe is overlapped: {e}") from e
-
         def _get_incoming_message(self) -> Any:
-            """
-            Get the incoming message from the pipe
 
-            :param Pipehandle: Handle of the pipe
-            :return: message received
-            """
+            assert self.pipe_handle is not None
+            buffer_size = 1024  # 1KB at a time
+            data = ""
+
             try:
-                assert self.pipe_handle is not None
+                while True:
+                    result, tmp = win32file.ReadFile(
+                        self.pipe_handle.handle, 
+                        buffer_size
+                    )
+                    if tmp:
+                        data += tmp.decode("utf-8") # type: ignore
 
-                buffer_size = 4096  # For example, 4 KB
-                if self._is_pipe_overlapped():
-                    # Allocate a buffer for the data
-                    read_buffer = win32file.AllocateReadBuffer(buffer_size)
-
-                    # Create an OVERLAPPED structure
-                    overlapped = pywintypes.OVERLAPPED()
-
-                    # Create an event for the OVERLAPPED structure
-                    overlapped.hEvent = win32event.CreateEvent(None, True, False, None)
-
-                    # Start the overlapped read operation
-                    err_code, _ = win32file.ReadFile(self.pipe_handle.handle, read_buffer, overlapped)
-
-                    # If the operation is pending, wait for it to complete
-                    if err_code == winerror.ERROR_IO_PENDING:
-                        timeout = 100  # milliseconds
-                        win32event.WaitForSingleObject(overlapped.hEvent, timeout)
-
-                        # Get the result of the overlapped operation
-                        n_bytes_read = win32file.GetOverlappedResult(
-                            self.pipe_handle.handle, overlapped, True
-                        )
-
-                        # Extract the data from the buffer
-                        data = bytes(read_buffer[:n_bytes_read]).decode("utf-8")  # type: ignore
-
-                    elif err_code == win32event.WAIT_TIMEOUT:
-                        raise TimeoutError("Timeout waiting for overlapped operation to complete")
-
-                else:
-                    # Read the data from the pipe
-                    _, data = win32file.ReadFile(self.pipe_handle.handle, buffer_size)
-
-                # Load the data as json and return
-                return json.loads(data)
-
+                    if result is not winerror.ERROR_MORE_DATA:
+                        break
+            
             except pywintypes.error as e:
-                raise BrokenPipeError(f"Error receiving message from named pipe: {e}") from e
+                raise BrokenPipeError(f"{e.strerror}") from e
 
+            finally:
+                # Load the data as json and return
+                if data:
+                    data = json.loads(data)
+
+            return data
+                
         def _send_message(self, message: Any) -> None:
             """
             Send the message to the pipe
@@ -159,12 +117,16 @@ else:
                     0,
                     None,
                     win32file.OPEN_EXISTING,
-                    win32file.FILE_FLAG_OVERLAPPED,
+                    0,
                     None,
                 )
+
                 if self.pipe_handle:
                     win32pipe.SetNamedPipeHandleState(
-                        self.pipe_handle.handle, win32pipe.PIPE_READMODE_MESSAGE, None, None
+                        self.pipe_handle.handle, 
+                        win32pipe.PIPE_READMODE_MESSAGE, 
+                        None, 
+                        None
                     )
 
                     clientConnected = True
