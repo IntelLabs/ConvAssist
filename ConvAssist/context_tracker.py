d4cbf2255c93bb38c51eb869a97bb2438b662745
Michael Beale <michael.beale@intel.com>
2024-09-25 09:50:43 -0700

    removed temporary edge case support

--- ConvAssist/context_tracker.py
+++ ConvAssist/context_tracker.py
@@ -31,27 +31,11 @@ class ContextTracker:
             return ""
 
     def get_tokens(self, count: int):
-        # TODO: FIXME Edge case - if there is only one token and it's a space return 0 tokens
-        if len(self.tokens) == 1 and self.tokens[0] == " ":
-            return 0, []
-
         actual_tokens = count if len(self.tokens) >= count else len(self.tokens)
         return actual_tokens, self.tokens[:actual_tokens]
 
     def get_last_token(self):
-        # filter out the last token if it is a space and return the next token
-        if self.tokens:
-            tmp = list(filter(lambda x: x != " ", self.tokens))
-            return tmp[-1] if tmp else ""
-        else:
-            return ""
-
-        # tmp = self.tokens.filter(lambda x: x != " ")
-
-        # last_token = self.tokens[-1] if len(self.tokens) > 0 else ""
-
-        # #TODO: FIXME - if the last token is a space, return an empty string
-        # return last_token if last_token != " " else ""
+        return self.tokens[-1] if len(self.tokens) > 0 else ""
 
     @property
     def context(self):
