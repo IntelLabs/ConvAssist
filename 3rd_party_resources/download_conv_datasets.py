6575461e275a5a8578c3ca0d43e9772f48c41cb1
Michael Beale <michael.beale@intel.com>
2024-12-16 11:49:47 -0800

    Add dataset download script and clean up: remove old download scripts for Human Conversation and Persona Chat datasets; update dependencies in pyproject.toml

---
+++ 3rd_party_resources/download_conv_datasets.py
@@ -0,0 +1,79 @@
+import kagglehub
+from tqdm import tqdm
+import pandas as pd
+import string
+import re
+import nltk
+from nltk.tokenize import sent_tokenize
+
+datasets = [
+    {
+        'NAME' : 'atharvjairath/personachat',
+        'DATASET_FILENAME' : 'personality.csv',
+        'OUTPUT_FILENAME' : './personachat/Persona_Chat_Data.txt',
+        'DELIMITER' : ',',
+        'COLS' : ['chat'],
+    },
+    {
+        'NAME' : 'projjal1/human-conversation-training-data',
+        'DATASET_FILENAME' : 'human_chat.txt',
+        'OUTPUT_FILENAME' : './human-conversation/Human_Conversation_Data.txt',
+        'DELIMITER' : ':',
+        'COLS' : [1],
+    }
+]
+
+def clean_sentence(sentence: str):
+    # Skip sentences containing redacted terms
+    if '<' in sentence and '>' in sentence:
+        return None
+    
+    # Create a translation table that removes punctuation except apostrophes
+    punct_to_remove = string.punctuation.replace("'", "")
+    trans_table = str.maketrans("", "", punct_to_remove)
+    cleaned = sentence.translate(trans_table)
+    cleaned = " ".join(cleaned.split())
+    cleaned = cleaned.replace(" ' ", " ")
+    
+    return f"{cleaned}\n"
+
+def parse(text):
+    sentences = []
+
+    try:
+        nltk.data.find('tokenizers/punkt')
+    except LookupError:
+        nltk.download('punkt')
+
+    pattern = r'\s([,.!?;:])'
+    text = re.sub(pattern, r'\1', text)
+
+    for sent in sent_tokenize(text):
+        sent = clean_sentence(sent)
+        if sent:
+            sentences.extend(sent)
+        else:
+            continue
+
+    return sentences
+
+for dataset in datasets:
+    # Download latest version
+    path = kagglehub.dataset_download(dataset['NAME'])
+
+    df = pd.read_csv(
+        f"{path}/{dataset['DATASET_FILENAME']}",
+        delimiter = dataset['DELIMITER'],
+        encoding = 'ascii',
+        encoding_errors = 'ignore',
+        usecols = dataset['COLS']
+        )
+
+    with open(dataset['OUTPUT_FILENAME'], 'w') as fout:
+        for row in tqdm(df.itertuples(index=False), desc="Processing sentences"):
+            sentences = parse(row[0])
+            previous_row = row[0]
+            if sentences:
+                fout.writelines(sentences)
+            else:
+                continue
